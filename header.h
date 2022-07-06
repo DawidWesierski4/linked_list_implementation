ed3f850a6b848abb4a29de08fa2faca5a35f0549
dawidwesierski4 <dawidwesierski4@gmail.com>
2022-07-06 13:46:09 +0200

    syntax changes

---
+++ header.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define NUMBER_OF_TPID_VALUES 3
+#define TPID_MAX_LENGHT 7
+
+struct linked_vlan_list
+{
+	unsigned short int vlan_id;
+	char tpid[TPID_MAX_LENGHT];
+	struct linked_vlan_list* next;
+};
+
+const char allowable_tpid_values[NUMBER_OF_TPID_VALUES][TPID_MAX_LENGHT] = {
+	"0x8100", "0x9100", "0x88a8"
+};
